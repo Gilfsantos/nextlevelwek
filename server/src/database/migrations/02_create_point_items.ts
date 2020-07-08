465b15a45d5e26380868e13ada4dba28846bc6ea
Gilmar <66905710+Gilfsantos@users.noreply.github.com>
2020-07-07 21:14:22 -0300

    criando banco dados
    criando sqlite

---
+++ server/src/database/migrations/02_create_point_items.ts
@@ -0,0 +1,7 @@
+export async function up() {
+    // criar a tabela
+}
+
+export async function down() {
+// voltar atras  (deletar tabela)
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,63 +1,9 @@
-import express, { request } from 'express'; 
+import express from 'express'; 
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/users', (request, response) => {
-   const search = String(request.query.search);
-
-   const filteredUsers = search ? users.filter(user => user.includes(search)): users;
-
-   console.log(search);
-// Rota Endereço completo da requisiçao
-// Recuros Qual entidade estamos acessando do sistema
-
-// get: Buscar uma ou mais informaçao do back end
-// post: Criar uma nova informaçao no back end
-// put: Atualizar uma informaçao existente no back end
-// delete: Remover uma informaçao do back end
-
-// post http://localhost:3333/users = criar um usuario
-// get  http://localhost:3333/users = listar usuarios
-// get  http://localhost:3333/users/5 = buscar dados do usuario com id 5
-
-
- // request param: parametros que vem na propria rota que identificam um recurso
- // query parame: parametros que vem na propria rota geralmente opcionais para filtros, paginaçao
- // request body : parametros para criaçao /atualizaaçao
-  return  response.json(filteredUsers);
-});
-const users = [
-    'Diego',
-    'Cleiton',
-    'Robson',
-    'Daniel'
- ];
-
-
-app.get('/users/:id', (request, response) => {
- const id = Number( request.params.id);
- const user = users[id];
-
- return response.json(user);
-});
-
-
-
-app.post('/users',(request,response) => {
-
-    const data = request.body;
-
-    console.log(data);
-
-    const user = {
-
-        name: data.name,
-        email: data.email,
-    };
-
-     return  response.json(user);
-});
+app.use(routes);
 
 app.listen(3333);
