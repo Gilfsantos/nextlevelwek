a8b8dfc42d26b40022b8fb114cf6ca1d6038b5d7
Gilmar <66905710+Gilfsantos@users.noreply.github.com>
2020-07-17 11:46:20 -0300

    criado primeiros itens
    itens

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,22 @@
 import express, {request, response} from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
 
 
 
-routes.get('/', (request, response) => {
-    return  response.json({message: 'Hello World'});
+routes.get('/itens', async(request, response) => {
+    const itens = await knex('itens').select('*');
+
+    const serializedItens = itens.map(itens => {
+        return {title: itens.title,
+            image_url:`http://localhost:3333/uploads/${itens.image}`,
+
+        }
+    })
+    // select * from itens
+    return  response.json(serializedItens);
 });
 
     export default routes;
